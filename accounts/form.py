6ceb289edbf9f879b3f63d370c4b8f5a4092450b
Gaurav Pandey <gaurav.pandey@smarten.com.np>
2025-10-08 22:27:13 +0545

    add user in admin panel

--- accounts/form.py
+++ accounts/form.py
@@ -8,6 +8,8 @@ User = get_user_model()
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
 
     class Meta:
         model = User
